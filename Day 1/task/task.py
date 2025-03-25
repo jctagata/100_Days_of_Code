fa0daae851039490b89fcf9ab2b79f08cb336479
Carlo Tagata <carlo@Carlos-MacBook-Pro.local>
2025-03-26 00:07:52 +0900

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+cityGrewUpIn = input("Which city did you grow up in?\n")
+petName = input("What is the name of your pet?\n")
+
+print("Your band name is " + cityGrewUpIn + " " +petName)
---
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,14 @@
+
+# String Type Check
+print(type("Hello"))
+
+# Integer Type Check
+print(type(123))
+
+# Float Type Check
+print(type(123.456))
+
+# Boolean Type Check
+print(type(True))
+
+# Conversion
