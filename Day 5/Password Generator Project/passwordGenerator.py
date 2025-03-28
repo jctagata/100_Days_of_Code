2e0c9e0fdd8965cbccbbc621f3850e7679eaf6d6
carlo <tagata.carlo@gmail.com>
2025-03-28 23:27:53 +0900

    Day 7 Completed

--- Day 5/Password Generator Project/passwordGenerator.py
+++
@@ -1,7 +0,0 @@
-import task
-
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-
---
+++ Day 6/Functions/task.py
@@ -0,0 +1,7 @@
+
+def my_function():
+    name = input("What is your name?\n")
+    print(f"Hello {name}!")
+
+
+my_function()
